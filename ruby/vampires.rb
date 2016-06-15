e16a49b6a4b6ff8415304972476f3e79b72c47dd
EphraimFriedman <ephraimfried@gmail.com>
2016-06-15 12:56:55 -0400

    added variables to get users input

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+#What is your name? 
+#How old are you? 
+# What year were you born? 
+# Would you like garlic bread? 
+#Would you like to enroll in the company’s health insurance? 
+
+puts "New Employee Form"
+
+puts "What is your name?"
+name= gets.chomp
+
+puts "How old are you?"
+age= gets.to_i
+
+puts "What year were you born?"
+year= gets.to_1
+
+puts "Would you like Garlic Bread on your menu?"
+garlic_bread= gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance= gets.chomp
