bef8e45bac35a52ee8b4e7812d16afbe65234b3b
EphraimFriedman <ephraimfried@gmail.com>
2016-06-27 17:23:32 -0400

    finished release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,15 @@
 class Santa
 
+	def initialize(gender,ethnicity)
+		puts "initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer",
+		 "Prancer", "Vixen", "Comet", "Cupid",
+		  "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -8,15 +18,39 @@ class Santa
 		puts "That was a good #{cookie_type}"
 	end
 
-	def initialize
-		puts "initializing Santa instance..."
+	def about
+		puts "Gender is: #{@gender}"
+		puts "Ethnicity is: #{@ethnicity}"
+		puts "reindeer ranking is: #{@reindeer_ranking}"
+		puts "Age is #{@age}"
 	end
 
 end
 
-harry = Santa.new
+# harry = Santa.new
 
-harry.speak
+# harry.speak
 
-harry.eat_milk_and_cookies("chocolate chip")
+# harry.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+santas << Santa.new("male", "Caucasian")
+
+p santas[2]
+
+
+
+
+
+
+
+
+
 
