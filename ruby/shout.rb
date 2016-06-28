07be9ce11f714a60048266f0d5030bd818fdf1a2
EphraimFriedman <ephraimfried@gmail.com>
2016-06-28 16:45:47 -0400

    added mixin module to classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,43 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(word)
+#   	response = (word + "! ") * 8
+#   	response.rstrip!
+#   end
+# end
+
+# p Shout.yell_angrily("Crazy")
+
+# p Shout.yelling_happily("Horay")
+
 module Shout
-  def self.yell_angrily(words)
+def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(word)
+  def yelling_happily(word)
   	response = (word + "! ") * 8
   	response.rstrip!
   end
 end
 
-p Shout.yell_angrily("Crazy")
+class Sport_Fans
+	include Shout	
+end
 
-p Shout.yelling_happily("Horay")
+class Programmers
+	include Shout
+end
+
+fan = Sport_Fans.new
+
+p fan.yelling_happily("Score")
+p fan.yell_angrily("Booo")
+
+programer = Programmers.new
+
+p programer.yell_angrily("This code ain't working")
+p programer.yelling_happily("Finally")
