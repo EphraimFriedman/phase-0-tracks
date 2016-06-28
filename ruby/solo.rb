4b4726a9e35742f90cb07a9b07b397d5d171e1d2
EphraimFriedman <ephraimfried@gmail.com>
2016-06-28 11:48:56 -0400

    designed car class

---
+++ ruby/solo.rb
@@ -0,0 +1,20 @@
+# a car class
+	
+# 		method initialize
+# 			input: will be color, year and size
+# 			give it an attribute of any color
+# 			and any year
+# 			and any size
+
+# 		method to honk horn
+# 			print honk 3 times
+
+# 		method gas type
+# 			input: the gas type
+# 			print please fill the tank with gas type
+
+# 		method too fast
+# 			input: speed limit and speed
+# 			IF the user is going faster than the speed limit
+# 			print too fast
+# 			Otherwise print you are a good driver
