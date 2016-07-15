ede224cb807f721e500cb7841b7727dc669b79c4
EphraimFriedman <ephraimfried@gmail.com>
2016-07-15 14:14:01 -0400

    create function to generate random array

--- js/algos.js
+++ js/algos.js
@@ -30,20 +30,20 @@
 // // 	return false
 
 
-function matching_object(object1, object2) {
+// function matching_object(object1, object2) {
 
-	for (var key in object1) {
+// 	for (var key in object1) {
 		 
-		 if (object1[key] == object2[key]) {
-		 	result = true
-		 	{break}
-		 }
-		 else {
-		 	result = false
-		 }
-	}
-	return result
-}
+// 		 if (object1[key] == object2[key]) {
+// 		 	result = true
+// 		 	{break}
+// 		 }
+// 		 else {
+// 		 	result = false
+// 		 }
+// 	}
+// 	return result
+// }
 
 
 // // DRIVER CODE
@@ -54,19 +54,44 @@ function matching_object(object1, object2) {
 // console.log(find_longest_word(["one", "two", "three", "four"]))
 
 
-// test matching object function
-h1 = {name: "bob", age: 27, food: "fast"}
-h2 = {name: "bob", age: 23, boot: "fast"}
+// // test matching object function
+// h1 = {name: "bob", age: 27, food: "fast"}
+// h2 = {name: "bob", age: 23, boot: "fast"}
 
-console.log(matching_object(h1, h2))
+// console.log(matching_object(h1, h2))
 
-console.log(matching_object({color: "blue", eyes: "big", head: "small"}, {color: "green", eyes: "big", head: "large"}))
+// console.log(matching_object({color: "blue", eyes: "big", head: "small"}, {color: "green", eyes: "big", head: "large"}))
 
-console.log(matching_object({word: "hello", sign: "turn left"}, {word: "bye", sign: "turn right"}))
+// console.log(matching_object({word: "hello", sign: "turn left"}, {word: "bye", sign: "turn right"}))
 
-console.log(matching_object({greet: "hello", left_or_right: "turn left"}, {greeting: "bye", direction: "turn right"}))
+// console.log(matching_object({greet: "hello", left_or_right: "turn left"}, {greeting: "bye", direction: "turn right"}))
 
 		
+// generate random array
+
+// take an iteger 
+// create empty array
+// loop the iteger amount of times
+// 	create random word with a length between 1 and 10
+// 	push new word into array
+// return new array
+
+function generate_array(number) {
+var array_of_random_words = []
+var	random_string = "qwertyuiopasdfghjk"
+
+
+for (var i = 0; i < number; i++) {
+	var new_word = random_string.substring(Math.floor(Math.random() * 10 + 1), Math.floor(Math.random() * 10 + 1))
+	array_of_random_words.push(new_word)
+}
+return array_of_random_words
+}
+
+
+console.log(generate_array(5))
+
+console.log(generate_array(2))
 
 
 
