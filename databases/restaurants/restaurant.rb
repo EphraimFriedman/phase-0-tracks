4d3e0e9ad725b07667b03af34c8a85c884f31692
EphraimFriedman <ephraimfried@gmail.com>
2016-07-21 16:25:42 -0400

    create update method to update restaurant info

--- databases/restaurants/restaurant.rb
+++ databases/restaurants/restaurant.rb
@@ -14,21 +14,29 @@ create_table = <<-SQL
 	)
 SQL
 restaurant_database.execute(create_table)
-
+# add restaurant method
 def add(database, name, cuisine, price)
 	database.execute("INSERT INTO restaurants (name, cuisine, price) VALUES (?,?,?)", [name, cuisine, price])
 end
 
-
-
+# delete restaurant method
 def delete(database, name)
 	database.execute(<<-SQL 
-		DELETE FROM restaurants WHERE name="Basil" 
+			DELETE FROM restaurants WHERE name="#{name}"
 		SQL
 		)
+end
+
+def update(database, current_name, catagory, update_info)
+	database.execute(<<-SQL 
+		UPDATE restaurants SET "#{catagory}"="#{update_info}" WHERE name="#{current_name}" 
+		SQL
+		)	
 end
 
 # DRIVER CODE
 # add(restaurant_database, "Kingston Pizza", "fast food", 5)
 # add(restaurant_database, "Basil", "French", 30)
 # delete(restaurant_database, "Basil")
+update(restaurant_database, "Kingston Pizza", "price", 7)
+update(restaurant_database, "Kingston Pizza", "name", "King")
