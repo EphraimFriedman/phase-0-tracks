41c1f0e9eabc96feeb41a7b39d0db16ce3700a6d
EphraimFriedman <ephraimfried@gmail.com>
2016-07-21 17:23:37 -0400

    add print method

--- databases/restaurants/restaurant.rb
+++ databases/restaurants/restaurant.rb
@@ -40,12 +40,8 @@ def search_by_name(database, name)
 			SELECT * FROM restaurants WHERE name="#{name}"
 		SQL
 		) 
-	search_result
-	# puts "----- SEARCH BY NAME -----"
-	# result.each do |restaurant|
-	# 		p restaurant
-	# 		puts "#{restaurant["name"]} \t \t #{restaurant["cuisine"]} \t\t #{restaurant["price"]}"
-	# 	end
+	print_search(search_result)
+	
 end
 
 def search_by_cuisine(database, cuisine)
@@ -53,7 +49,7 @@ def search_by_cuisine(database, cuisine)
 			SELECT * FROM restaurants WHERE cuisine="#{cuisine}"
 		SQL
 		) 
-	search_result
+	print_search(search_result)
 end
 
 def search_by_price(database, price)
@@ -61,21 +57,39 @@ def search_by_price(database, price)
 			SELECT * FROM restaurants WHERE price< "#{price}"
 		SQL
 		) 
-	search_result
+	print_search(search_result)
 end
 
-def method_name
-	
+def print_search(result)
+	puts "----- SEARCH BY NAME -----"
+	result.each do |restaurant|
+			# p restaurant
+			puts "#{restaurant["name"]} \t \t #{restaurant["cuisine"]} \t\t #{restaurant["price"]}"
+		end
 end
 
+def print_list(database)
+	full_database = database.execute("SELECT * FROM restaurants")	
+	full_database.each do |restaurant|
+			# p restaurant
+			puts "#{restaurant["name"]} \t \t #{restaurant["cuisine"]} \t\t #{restaurant["price"]}"
+		end
+end
 
 
 # # DRIVER CODE
-add(restaurant_database, "Kingston Pizza", "fast food", 5)
-add(restaurant_database, "Basil", "French", 30)
-# delete(restaurant_database, "Basil")
-# # update(restaurant_database, "Kingston Pizza", "price", 7)
-# # update(restaurant_database, "Kingston Pizza", "name", "King")
+# # search_result = nil
+# add(restaurant_database, "Kingston Pizza", "fast food", 5)
+# add(restaurant_database, "Basil", "French", 30)
+# # delete(restaurant_database, "Basil")
+# # # update(restaurant_database, "Kingston Pizza", "price", 7)
+# # # update(restaurant_database, "Kingston Pizza", "name", "King")
+
 # search_by_name(restaurant_database, "Basil")
-# p search_by_cuisine(restaurant_database, "fast food")
-p search_by_price(restaurant_database, 20)
+
+
+# search_by_cuisine(restaurant_database, "fast food")
+
+# search_by_price(restaurant_database, 20)
+
+print_list(restaurant_database)
